5923f26edb09e09c3d88749f9014eeeedb255723
dataexplorerRahul <rahulmenon1758@gmail.com>
2025-07-09 06:36:54 +0530

    Updates README, pushes vectorstore files, adds pages to streamlit app

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# devpost_hackathon_MamaMind
+## MamaMind - Gentle Guidance for New Beginnings App
+
+## About: 
+MamaMind is an intelligent mental health chatbot designed to provide support and assistance to individuals experiencing perinatal depression. The application offers a range of functionalities tailored to help users understand, manage, and alleviate their mental health challenges through a friendly and supportive interface. Here's a breakdown of its key features and capabilities:
+
+1. **Welcome and Initial Interaction**
+Upon launching the application, users are greeted with a warm welcome message that sets a comforting tone for their interaction. The chatbot introduces itself and offers users the option to engage in a mental health assessment through the Edinburgh Postnatal Depression Scale (EPDS) questionnaire.
+
+2. **EPDS Questionnaire**
+If users choose to proceed with the EPDS questionnaire, MamaMind guides them through a series of questions designed to evaluate their depression severity. The responses are collected, scored, and analyzed to provide an accurate assessment of the user's mental health status.
+
+3. **Severity-Based Guidance**
+Based on the results of the EPDS questionnaire, MamaMind categorizes the severity of the user's depression and offers tailored guidance and resources. This personalized approach ensures that users receive appropriate advice and support based on their specific needs.
+
+4. **Interactive Chat Support**
+Users can engage in an open-ended conversation with MamaMind by typing their concerns and questions into the chat input. The chatbot leverages advanced natural language processing techniques to understand the user's input and provide relevant responses. It uses knowledge of cognitive behavioral therapy, meditation techniques, mindfulness practices, and other therapeutic methods to guide users through their feelings and improve their well-being.
+
+5. **Contextual Responses**
+MamaMind employs a Retrieval-Augmented Generation (RAG) with query decomposition method to retrieve relevant information from a pre-built knowledge base of 84 PubMed articles, guideline documents recommended by OECD member countries related to perinatal depression and reliable resources such as WHO and NIMH. This ensures that the responses are contextually accurate and informative. The chatbot uses the retrieved context, along with the user's input and severity level, to generate comprehensive and helpful responses.
+
+6. **Friendly Closure**
+When users decide to end the conversation, MamaMind ensures a friendly and supportive closure. It responds positively to expressions of gratitude and offers a parting message that encourages users to return whenever they need further assistance.
+
+7. **Continuous Interaction**
+MamaMind is designed to facilitate continuous interaction. It asks relevant follow-up questions to maintain the conversation flow and provide ongoing support. The chatbot remains available to answer any new questions or concerns users might have.
+
+**Key Benefits**
+**Personalized Support:** Tailored advice and resources based on the user's depression severity.
+**Expert Guidance:** Incorporates the knowledge base from the Pubmed research articles on perinatal depression and the guideline documents recommended by member countries of OECD. 
+**User-Friendly Interface:** Easy-to-use chat interface.
+**Contextual Accuracy:** Utilizes RAG using query decomposition to enhance the responses from the LLM and also ensuring accurate and reliable responses.
+**Ongoing Availability:** Always available for continuous support and interaction.
+MamaMind aims to create a safe and supportive environment for individuals experiencing perinatal depression, offering them the tools and resources needed to navigate their mental health journey.
+
+
+**Main Requirements:**
+Check the requirements.txt file for more information.
+
+* faiss-cpu
+* groq
+* huggingface-hub
+* langchain
+* langchain-community
+* langchain-core
+* langchain-groq
+* langchain-huggingface
+* langchain-text-splitters
+* python-dotenv
+* sentence-transformers
+* streamlit
+* streamlit-extras
+* transformers
+
+**Installation:**
+
+1. Clone this repository.
+2. Create a virtual environment:
+    ```bash
+    python -m venv venv
+    source venv/bin/activate
+    ```
+3. Install required dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## **Running the App:**
+
+1. Open the project in your preferred IDE (e.g., VS Code).
+2. Start the Streamlit app:
+    ```bash
+    streamlit run welcome.py
+    ```
+3. Access the app in your web browser: http://localhost:8501
+
+## **License:**
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## **Contact:**
+
+For any questions or feedback, please contact our team : nprinka235@gmail.com, rahulmenon1758@gmail.com, vaishnavim1311@gmail.com and foutse.yuehgoh@gmail.com.
+
+## Link to the streamlit application
+
+The application has been deployed on Streamlit community cloud. 
+Link ---> https://devposthackathonmamamind.streamlit.app/
+
+## Conclusion
+"MamaMind" is committed to providing effective, empathetic, and accessible mental health support for individuals experiencing perinatal depression. We are excited about the future and the potential to make a meaningful impact on the well-being of our users.
+
+As a ground-breaking tool for maternal mental health, "MamaMind" is an example of how technology can be used to provide empathetic and effective support and ensure that no woman is left alone with perinatal depression. This innovative approach not only enhances the well-being of mothers, but also contributes to healthier family dynamics and better developmental outcomes for children. "MamaMind" is a testament to the potential of AI-driven solutions in transforming healthcare, offering hope and support to those who need it most.
+
 
--- pages/Chat_with_MamaMind.py
+++ pages/Chat_with_MamaMind.py
@@ -1,64 +1,25 @@
 import os
-import time
-import json
-from dotenv import load_dotenv
 import streamlit as st
-from utils import *
 from textwrap import dedent
-from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage
+from utils import local_css, load_questions, load_vector_db, decompose_prompt, retrieve_and_generate, initialize_llm
 
 # Load environment variables
 load_dotenv()
 
+# Streamlit app settings
 st.set_page_config(page_title="MamaMind", page_icon=":female-doctor:")
-
-col1, col2 = st.columns([3, 1])
-
+# col1, col2 = st.columns([3, 1])
 with st.sidebar:
-    st.image("static\image1-removebg-preview.png")
-
+    st.image("static/image1-removebg-preview.png")
 # Apply custom CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
 local_css("styles.css")
 
-# Define constants
-DATA_PATH = 'data/'
-DB_FAISS_PATH = 'vectorstore/db_faiss'
-SYSTEM_PROMPT = dedent("""
-    You are a mental health adviser. You should use your knowledge of cognitive behavioral therapy,
-    meditation techniques, mindfulness practices, and other therapeutic methods to guide the user through 
-    their feelings and improve their well-being. 
-    You will respond to the user's questions based on the {input}, {severity} of their perinatal depression.
+# Folder path
+FAISS_DB_PATH = './vectorstore'
 
-    * For mild severity, offer supportive advice and practical coping strategies.
-    * For moderate severity, provide empathetic responses and suggest helpful resources.
-    * For severe severity, advise the user to seek professional help and share urgent resources.
-    After addressing the user's question, include a relevant follow-up question only if it feels natural to continue the conversation. 
-    Avoid overwhelming the user with too many questions.
-
-    If the conversation ends with 'thank you,' 'thanks,' 'bye,' or 'goodbye,' conclude in a friendly manner with: 
-    'I hope this helps. If you have more questions, feel free to ask!'
-    
-    "Context: {context}"
-    "Input: {input},{severity}"
-    """
-)
-
-# Function to load questions from the JSON file
-def load_questions(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
-    
+# Function to     
 def display_welcome_message(llm):
     prompt = """
             You are an AI assistant designed to welcome users to the "MamaMind-Gentle Guidance for new beginnings" app.
@@ -82,13 +43,9 @@ def display_welcome_message(llm):
             Please share your questions or concerns, and let us provide the support you need.
             ---
             """
-    response = llm.invoke(prompt)
-    message_text = response.content
-    lines = message_text.split(" ")
-    for line in lines:
-        yield line + " "
-        time.sleep(0.02)
+    return llm.stream(prompt)
 
+# Function to return Severity of user's depression
 def interpret_epds_score(score):
     if score <= 9:
         return "None to minimal depression"
@@ -99,178 +56,148 @@ def interpret_epds_score(score):
     else:
         return "Severe depression"
 
-def begin_chat(llm, severity):
+def begin_chat(llm, retriever, severity):
     with st.chat_message("assistant"):
-        st.write("How can I help you?")
+        st.write('How can I help you?')
+
+    # Display chat_history
+    for message in st.session_state.chat_history:
+        if isinstance(message, HumanMessage):
+            with st.chat_message("user"):
+                st.markdown(message.content)
+        else:
+            with st.chat_message("assistant"):
+                st.markdown(message.content)
     
     query = st.chat_input("Tell me about your problems")
 
-    if query:
-        if os.path.exists(DB_FAISS_PATH):
-            context_docs = get_similar_docs(query)
-            context = " ".join(context_docs.page_content)
-            with col2:
-                st.write("RAG output")
-                # st.markdown(context_docs.page_content)
-                st.markdown(context_docs.metadata)
-        else:
-            directory = './data'
-            documents = load_docs(directory)
-            docs = split_docs(documents)
-            generate_embeddings(docs)
-
-
-        # Initialize LLM
-        llm = llm
-        
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", SYSTEM_PROMPT),
-                ("human", "{input}"),
-            ]
-        )
-        
-        # Create QA chain
-        db = FAISS.load_local(DB_FAISS_PATH, HuggingFaceEmbeddings(model_name="NeuML/pubmedbert-base-embeddings", model_kwargs={'device': 'cpu'}), allow_dangerous_deserialization=True)
-        qa_chain = create_qa_chain(llm, db, prompt)
+    if query is not None and query != "":
+        # Display the query
+        with st.chat_message('user') :
+            st.markdown(query)
+            
+        # Decompose the question
+        decomposed_questions = decompose_prompt(query, llm)
 
         # Get response
-        response = qa_chain.invoke({"context": context, "input": query, "severity": severity})
-        answer = response['answer']
+        with st.chat_message('assistant'):
+            answer = st.write_stream(retrieve_and_generate(decomposed_questions, retriever, llm, chat_history=st.session_state.chat_history, severity=severity))
 
-        # Insert new messages at the beginning of the list
-        st.session_state.messages.insert(0, {"role": "assistant", "content": answer})
-        st.session_state.messages.insert(0, {"role": "user", "content": query})
-
-        # Display messages
-        for message in st.session_state.messages:
-            if message["role"] == "user":
-                with st.chat_message("user"):
-                    st.write(message["content"])
-            else:
-                with st.chat_message("assistant"):
-                    st.write(message["content"])
+        # Append to chat history
+        st.session_state.chat_history.append(HumanMessage(query))
+        st.session_state.chat_history.append(AIMessage(answer))
 
 def main():    
     
     # Get user's Groq api key
     with st.sidebar:
         groq_api_key = st.text_input(label = "**Groq API key**", placeholder="Ex gsk-2twmA8tfCb8un4...",
-        key ="groq_api_key_input", help = "How to get a Groq api key: Visit https://console.groq.com/login")
+        key ="groq_api_key_input", help = "How to get a Groq api key: Visit https://console.groq.com/login", type="password")
 
         # Initialize session state for the model if it doesn't already exist
         if 'selected_model' not in st.session_state:
             st.session_state['selected_model'] = ""
         # Container for markdown text
         with st.container():
-            st.markdown("""Make sure you have entered your API key.
-                        Don't have an API key yet?
-                        Visit https://console.groq.com/login and Get your API key""")
-            st.session_state['selected_model'] = st.selectbox("Choose the model",("","llama3-70b-8192","gemma2-9b-it"),key="tab2_sidebar_selectbox")
-            model_chosen = st.session_state['selected_model']
-        if model_chosen:
-            st.write(f"You have selected the model: {model_chosen}")
-        else:
-            st.write("Please select a model.")
-
-    with col1:
-        st.markdown("<div style='text-align: center; font-size: 32px; font-weight: bold;'>👩‍⚕️ MamaMind</div>", unsafe_allow_html=True)
-        st.markdown("<div style='text-align: center; font-size: 24px;'>Gentle Guidance for New Beginnings</div", unsafe_allow_html=True)
-        st.divider() 
-
-        if groq_api_key and model_chosen:
-            
-            llm = initialize_llm(model_chosen, groq_api_key)
-
-            # Initialize session state for messages
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
-            if "query" not in st.session_state:
-                st.session_state.query = ""
-            # Initialize session state to keep track of question index and responses
-            if 'started' not in st.session_state:
-                st.session_state.started = False
-            if 'question_index' not in st.session_state:
-                st.session_state.question_index = 0  # to start from the first question in the list
-            if 'responses' not in st.session_state:
-                st.session_state.responses = []
-            if 'scores' not in st.session_state:
-                st.session_state.scores = []
-
-            # Load the EPDS questions
-            epds_questions = load_questions('static/epds_questions.json')
-            if "welcome_message_displayed" not in st.session_state:
-                #Generate Welcome Message with User Input
-                with st.chat_message("assistant"):
-                    llm = llm
-                    st.write_stream(display_welcome_message(llm))
-                    st.session_state["welcome_message_displayed"] = True
-
-            # Function to move to the next question
-            def next_question(response, score):
-                st.session_state.responses.append(response)
-                st.session_state.scores.append(score)
-                if st.session_state.question_index < len(epds_questions) - 1:
-                    st.session_state.question_index += 1
-                else:
-                    st.session_state.question_index = 'completed'
-
-            # Ask if the user wants to start the questionnaire
-            if not st.session_state.started:
-                with st.chat_message("assistant"):
-                    # st.write("To assess the level of severity of the depression, we would like you to answer the Ediburgh Depression questionnaire.")
-                    start = st.radio("Would you like to answer the EPDS questionnaire?", ("Yes", "No"), index=None, horizontal=True, key='start_radio')
-                #st.write("Start: ", start)
-                if start == "Yes":
-                    st.session_state.started = True
-                    st.session_state.question_index = 0
-                    st.session_state.responses = []
-                    st.session_state.scores = []
-                    st.rerun()
-                elif start == "No":
-                    st.session_state.started = False
-                    severity = "None"
-                    
-                    st.write("Thank you for your time. Ask any questions you have to MamaMind 🙂.")
-                    begin_chat(llm, severity)
-            else:
-                # st.write("Session state started: ", st.session_state.started)
-                # st.write("Question index: ", st.session_state.question_index)
-                if st.session_state.question_index == 'completed':
-                    with st.chat_message('assistant'):
-                        st.write("You have completed the EPDS questionnaire.")
-                    epds_score = sum(st.session_state.scores)
-                    severity = interpret_epds_score(epds_score)
-                    answer = llm.invoke(severity)
-                    with st.chat_message('assistant'):
-                        st.write(answer.content)
+            st.markdown("Make sure you have entered your API key")
+            model_chosen = st.selectbox("Choose the model", ("llama3-70b-8192","mixtral-8x7b-32768"), key="tab2_sidebar_selectbox", index=None)
+            st.session_state['selected_model'] = model_chosen
     
-                    begin_chat(llm, severity)
-                    # st.write("Your total score:", epds_score)
-                    # st.write("Severity:", severity)
-                else:
-                    current_question = epds_questions[st.session_state.question_index]
-                    with st.chat_message("assistant"):
-                        st.write(current_question["question"])
-                        response = st.radio("Select your response:", current_question["options"], index=None, horizontal=True)
-                    if st.button("**Next**"):
-                        if response:  # Ensure a response is selected
-                            score = current_question["scores"][current_question["options"].index(response)]
-                            next_question(response, score)
-                            st.rerun()
-                        else:
-                            st.warning("Please select a response before proceeding.")
+    st.markdown("<div style='text-align: center; font-size: 32px; font-weight: bold;'>👩‍⚕️ MamaMind</div>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: center; font-size: 24px;'>Gentle Guidance for New Beginnings</div", unsafe_allow_html=True)
+    st.divider() 
 
-    with col2:
-        st.write("Example Prompts")
-        with st.expander("View Example Prompts"):
-            st.markdown("""
-            - **How can I manage my anxiety during pregnancy?**
-            - **What are some coping strategies for postpartum depression?**
-            - **Can you suggest activities to improve my mental health?**
-            - **What resources are available for new mothers?**
-            - **How do I recognize the signs of perinatal depression?**
-            """)
+    if groq_api_key and model_chosen:
+
+        llm = initialize_llm(model_chosen, groq_api_key)
+
+        # Load the EPDS questionnaire
+        epds_questions = load_questions('static/epds_questions.json')
+
+        # Load Vector store DB & Retriever
+        vectorstore = load_vector_db(FAISS_DB_PATH)
+        retriever = vectorstore.as_retriever(search_kwargs={'k':3})
+
+        # Initialize session state for messages
+        if "chat_history" not in st.session_state:
+            st.session_state.chat_history = []
+        if "query" not in st.session_state:
+            st.session_state.query = ""
+
+        # Initialize session state to keep track of question index and responses
+        if 'started' not in st.session_state:
+            st.session_state.started = False
+        if 'question_index' not in st.session_state:
+            st.session_state.question_index = 0  # to start from the first question in the list
+        if 'responses' not in st.session_state:
+            st.session_state.responses = []
+        if 'scores' not in st.session_state:
+            st.session_state.scores = []
+
+        #Generate Welcome Message with User Input
+        if "welcome_message_displayed" not in st.session_state:
+            with st.chat_message("assistant"):
+                st.write_stream(display_welcome_message(llm))
+                st.session_state["welcome_message_displayed"] = True
+
+        # Function to move to the next question
+        def next_question(response, score):
+            st.session_state.responses.append(response)
+            st.session_state.scores.append(score)
+            if st.session_state.question_index < len(epds_questions) - 1:
+                st.session_state.question_index += 1
+            else:
+                st.session_state.question_index = 'completed'
+
+        # Ask if the user wants to start the questionnaire
+        if not st.session_state.started:
+            with st.chat_message("assistant"):
+                start = st.radio("Would you like to answer the EPDS questionnaire?", ("Yes", "No"), index=None, horizontal=True, key='start_radio')
+            if start == "Yes":                    
+                st.session_state.started = True
+                st.session_state.question_index = 0
+                st.session_state.responses = []
+                st.session_state.scores = []
+                st.rerun()
+            elif start == "No":
+                st.session_state.started = False
+                severity = "Not provided"
+                with st.chat_message('assistant'):
+                    st.write("No problem. Please ask any questions you have to MamaMind 🙂.")
+                begin_chat(llm, retriever, severity)
+        else:
+            if st.session_state.question_index == 'completed':
+                with st.chat_message('assistant'):
+                    st.write("You have completed the EPDS questionnaire.")
+                epds_score = sum(st.session_state.scores)
+                severity = interpret_epds_score(epds_score)
+                begin_chat(llm, retriever, severity)
+            else:
+                current_question = epds_questions[st.session_state.question_index]
+                st.write("Click on the options that comes closest to how you have felt IN THE PAST 7 DAYS—not just how you feel today.\
+                        Complete all 10 questions.\n\
+                        Kindly Note: This is a screening test; not a medical diagnosis.")
+                with st.chat_message("assistant"):
+                    st.write(current_question["question"])
+                    response = st.radio("Select your response:", current_question["options"], index=None, horizontal=False)
+                if st.button("**Next**"):
+                    if response:  # Ensure a response is selected
+                        score = current_question["scores"][current_question["options"].index(response)]
+                        next_question(response, score)
+                        st.rerun()
+                    else:
+                        st.warning("Please select a response before proceeding.")
+
+    # with col2:
+    #     st.write("Example Prompts")
+    #     with st.expander("View Example Prompts"):
+    #         st.markdown("""
+    #         - **How can I manage my anxiety during pregnancy?**
+    #         - **What are some coping strategies for postpartum depression?**
+    #         - **Can you suggest activities to improve my mental health?**
+    #         - **What resources are available for new mothers?**
+    #         - **How do I recognize the signs of perinatal depression?**
+    #         """)
         
 if __name__ == "__main__":
     main()
